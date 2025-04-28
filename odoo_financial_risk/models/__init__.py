6e1b62a33daab1466e066486687dc8d73a7db80b
Jason Zou <zou.jason@qq.com>
2025-04-28 12:44:18 +0800

    去掉主master分支财务风控模块

--- odoo_financial_risk/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import base_data
-from . import taxation_economic_contract_data
-from . import taxation_daily_research_develop
-from . import taxation_invoice_cloud_data
-from . import account_analysis_supplier_outside
-from . import account_analysis_travel_indicators
-from . import dues_income_expenditure_sheet
-
